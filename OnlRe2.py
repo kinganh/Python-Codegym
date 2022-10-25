ef1d1e4a94423f1780aaa167a04d2df2348bf488
kinganh <105800310+kinganh@users.noreply.github.com>
2022-10-25 17:06:41 +0700

    Add files via upload

--- OnlRe2.py
+++ OnlRe2.py
@@ -1,66 +1,35 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import sklearn
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder, OrdinalEncoder
+import datetime
 
-df = pd.read_csv('Data\OnlineRetail.csv', encoding = 'ISO-8859-1')
-print(df.shape)
-df.info()
+df = pd.read_csv('Data\OnlineRetail.csv', encoding='ISO-8859-1')
 
-df.isna()
+# Biểu đồ đường thể hiện doanh thu theo từng tháng năm 2011.
+# Biểu đồ cột thể hiện số lượng đơn hàng trong các tháng của năm 2011.
 
-df = df[df['Quantity'] >= 0]
+df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate']) #chuyển InvoiceDate thành datetime object
+d1 = df[['InvoiceNo', 'InvoiceDate', 'Quantity', 'UnitPrice']] #lấy các cột dữ liệu cần thiết
+d1['Revenue'] = d1['Quantity'] * d1['UnitPrice'] #tính doanh thu trên mỗi row
+d1 = d1.set_index(['InvoiceDate']) #chuyển mỗi giá trị InvoiceDate thành index của mỗi để search theo index
+d2 = d1['2011'] #lọc những hàng mà index có chứa '2011'
+d2 = d2.reset_index()
+d3 = d2.groupby(by=d2['InvoiceDate'].dt.month).sum() #tính tổng doanh thu theo tháng
 
-df1 = df.dropna()
-print(df1.shape)
-df1.info()
+d4 = d1.drop_duplicates(subset = 'InvoiceNo', keep = 'first') # xóa bỏ các dòng trùng lặp của cùng một đơn hàng
+d4 = d4['2011']
+d4 = d4.reset_index()
+d5 = d4.groupby(by=d4['InvoiceDate'].dt.month).count() #đếm tổng số đơn hàng trong tháng
 
-df2 = df.dropna(how='all') # xóa dòng khuyết thuyết hoàn toàn
-df3 = df.dropna(thresh=7)  # xóa dòng có ít nhất 7 giá trị không khuyết thiếu
-df4 = df.dropna(subset='CustomerID') # xóa dòng có giá trị khuyết thiếu ở cột CustomerID
-df5 = df
-df5['CustomerID'] = df5['CustomerID'].fillna(-1)
-df5['Country'] = df5['Country'].fillna(method='ffill') #fronfill
+x = d5.index.get_level_values(0)
+plt.bar(x, d5['InvoiceNo'], width = 0.5, label = 'InvoiceNo')
+axes1 = plt.gca()
+axes2 = axes1.twinx()
+axes2.plot(x, d3['Revenue'], label = 'Revenue', linewidth = 2, c = 'r', marker = 'o')
+axes1.set_xlabel('Month', fontsize = 14)
+axes1.set_ylabel('#Invoices', fontsize = 14)
+axes2.set_ylabel('$', fontsize = 14)
 
-##### Xử lý dữ liệu ngoại lại
-sns.boxplot(x= df1['Quantity'])
+axes1.legend(fontsize = 14)
+axes2.legend(fontsize = 14)
+plt.title('#Invoices and Revenue in 2011', fontsize = 16)
 plt.show()
-
-Q1 = df1['Quantity'].quantile(0.25)
-Q3 = df1['Quantity'].quantile(0.75)
-IQR = Q3 - Q1
-
-df1 = df1[~((df1['Quantity'] < (Q1 - 1.5*IQR)) | (df1['Quantity'] > (Q3 + 1.5*IQR)))]
-sns.boxplot(x= df1['Quantity'])
-plt.show()
-
-##### Chuẩn hóa dữ liệu
-standard = StandardScaler()
-df_s1 = standard.fit_transform(df1[['Quantity']])
-df_s1 = pd.DataFrame(df_s1)
-df_s1.boxplot()
-plt.show()
-sns.kdeplot(data=df_s1)
-plt.show()
-print(df_s1)
-MinMax = MinMaxScaler()
-Robust = RobustScaler()
-
-##### Mã hóa dữ liệu
-OneHot = OneHotEncoder()
-coded_data1 = OneHot.fit_transform(np.asarray(df1['Country']))
-coded_data1.todense()
-pd.get_dummies(df1['Country']) # mã hóa bằng Pandas
-
-Label = LabelEncoder()
-coded_data2 = Label.fit_transform(np.asarray(df1['Country']))
-df1['Country'].astype('category').cat.codes # mã hóa bằng Pandas
-
-##### Rời rạc hóa dữ liệu
-cut = pd.cut(df['Quantity'], 5)
-pd.value_counts(cut)
-qcut= pd.qcut(df['Quantity'], 5)
-pd.value_counts(qcut)
